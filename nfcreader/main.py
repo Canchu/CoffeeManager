bca6f0f7226e338d9baf999b11ba0da3a0164885
uhey <uhey@redimpulz.com>
2016-05-29 01:18:03 +0900

    getリクエスト送信

--- nfcreader/main.py
+++ nfcreader/main.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import time
+import requests
+
+def get_username(nfc_id):
+	uri = 'http://127.0.0.1:5000/get'
+	params = {'id': nfc_id}
+	response = requests.get(uri, params = params)
+	return response
 
 if __name__ == '__main__':
 	datetime_jst = time.strftime("%Y/%m/%d %H:%M:%S", time.localtime())
