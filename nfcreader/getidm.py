10d48c8642a915f72ef115fa3f2ec315abd379bb
Yuhei Tsutsui <uhey@redimpulz.com>
2016-06-02 05:48:19 +0900

    id取得

---
+++ nfcreader/getidm.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import nfc
+
+def connected(tag):
+    print tag.type
+    if isinstance(tag, nfc.tag.tt1.Type1Tag):
+        print str(tag.uid).encode("hex")
+    elif isinstance(tag, nfc.tag.tt2.Type2Tag):
+        print str(tag.uid).encode("hex")
+    elif isinstance(tag, nfc.tag.tt3.Type3Tag):
+        print 'IDm = 'str(tag.idm).encode("hex")
+    elif isinstance(tag, nfc.tag.tt4.Type4Tag):
+        print str(tag.uid).encode("hex")
+    else:
+        print "error: unknown tag type"
+
+def main():
+	clf = nfc.ContactlessFrontend('usb')
+	clf.connect(rdwr={'on-connect': connected})
+
+if __name__ == '__main__':
+	main()
