4e5da8ada3df43f639061f26bf9816a2db189344
MasakiAyano <masakiayano@MasakiAyano-no-MacBook-Pro.local>
2016-06-01 21:22:08 +0900

    getとpostに/で反応

--- manager/app.js
+++ manager/app.js
@@ -14,92 +14,63 @@ var users = require('./routes/users');
 var hello = require('./routes/hello');
 
 var app = express();
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
-server.listen(3000);
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+//var server = http.createServer(app);
+//var io = require('socket.io').listen(server);
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/hello', hello);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-//getパラメータを受けとり、からでなければDBに問い合わせ、ユーザー名を返す
-app.get('/',function(req, res)){
+app.get('/',function(req, res){
   var id = "";
 
-  if(req.query.id){
-    id = req.query.id;
+  if(!req.query.id){
+    console.log("get error!");
   }
+  else{
+    id = req.query.id;
 
-  //idでユーザー名をとってくる
-  var sql = "select name from test_id where id = '" + id +  "'";
+    //idでユーザー名をとってくる
+    var sql = "select name from test_id where id = '" + id +  "'";
 
-  //DBに接続
-  connection.query(sql, function (err, rows) {
-    name = rows;
-    console.log(rows);
-    
-    if(err){
+    //DBに接続
+    connection.query(sql, function (err, rows) {
+      
+      if(err){
         console.log("table SQL error!",err);
-    }
+      }
 
-    if(name == null || name == ""){
-        console.log("not registerd ID!");
-        return;
-    }
-    
-    console.log(name);
-    res.json({'name': name}); 
-}
+      if(rows != {}){
+         name = rows[0].name;
+      }
+      else{
+         name = "未登録IDです";
+      }
+      //res.render(name);
+      res.json({'name': name}); 
+    });
+  }
+});
 
-//postでJSONを読み込む
-
+//postでjsonという名前のパラメータで渡されてくるJSONを読み込む
 app.post('/', function(req, res){
-  console.log(req);
 
   if(req.body){
-    var json = req.body;
+    var json = req.body.json;
   }
   else{
     console.log("Error! post json cannot read");
     return;
   }
 
+  console.log(json);
   //jsonのデータをだす
-  var id = JSON.parse(json).ID;
-  var time = JSON.parse(json).Time;
-  var drink = JSON.parse(json).Drink;
+  var id = JSON.parse(json).id;
+  var date = JSON.parse(json).date;
+  var drink = JSON.parse(json).drink;
   var price;
+  var user_name;
 
   if(drink == 0){
     drink = "Barsita";
@@ -113,7 +84,7 @@ app.post('/', function(req, res){
      drink = "Dolce gusto w/ milk";
      price = 120;
   }
-  else if(drink == 3{
+  else if(drink == 3){
      drink = "Special T";
      price = 60;
   }
@@ -122,20 +93,49 @@ app.post('/', function(req, res){
     return;
   }
 
-  //id,date,drinkをjsonでpostするとデータベースを更新する
-  var insertSql = "INSERT INTO TEST VALUES('" +
-                    time  + "','"
-                    name  + "','"
-                    drink  + "','"
-                    price + "')'";
+  
+  //idでユーザ名をとってくる
 
-  connection.query(insertSql, function (err, rows) {
-    if(error){
-      console.log("insert data to DB failed.");
-      return;
-    }
+  var sql = "select name from test_id where id = '" + id +  "'";
+  connection.query(sql, function (err, rows) {
+      if(err){
+        console.log("table SQL error!",err);
+      }
+      if(rows != {}){
+         user_name = rows[0].name;
+      }
+      else{
+         user_name = "未登録IDです";
+      }
+
+    console.log(user_name);
+    //id,date,drinkをjsonでpostするとデータベースを更新する
+    var insertSql = "INSERT INTO TEST VALUES('" + date + "','"
+                    + user_name  + "','"
+                    + drink  + "','"
+                    + price + "');";
+    
+    connection.query(insertSql, function (err, rows) {
+      if(err){
+        console.log("insert data to DB failed.");
+        return;
+      }
+    });
+
   });
 });
+
+
+app.listen(3000);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/hello', hello);
+
 
 
 // production error handler
@@ -149,34 +149,12 @@ app.use(function(err, req, res, next) {
 });
 
 
-//connect DB
+//DB settings
 var connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'user',
   password: process.env.DB_PASS || 'NojiNoji',
   database: process.env.DB_NAME || 'CoffeeManager_db'
-});
-
-
-  
-// クライアントが接続してきたときの処理
-io.sockets.on('connection', function(socket) {
-    // メッセージを受けたときの処理
-    socket.on('message', function(data) {
-        //console.log(data.value);
-        // つながっているクライアント全員に送信
-        socket.broadcast.json.emit('message', { value: data.value });
-
-        console.log('Client sent us: ' + data.value);
-        sp.write(data.value, function(err, bytesWritten) {
-            console.log('bytes written: ', bytesWritten);
-        });
-    });
-
-    // クライアントが切断したときの処理
-    socket.on('disconnect', function(){
-        console.log("disconnect");
-    });
 });
 
 //connect with DB
@@ -188,5 +166,24 @@ connection.connect(function(err) {
   console.log('DBconnected as id ' + connection.threadId);
 });
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
 module.exports = app;
