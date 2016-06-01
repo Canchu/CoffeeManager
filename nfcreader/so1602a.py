d2d7c2b4c39c40e04b42d51a94cf17b0e2834217
uhey <uhey@redimpulz.com>
2016-06-02 04:29:35 +0900

    ref #8 LCD制御OK

--- nfcreader/so1602a.py
+++ nfcreader/so1602a.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import smbus
+import time
+
 class SO1602A():
 	"""docstring for SO1602A"""
 
-	import smbus
-	import time
 
 	def __init__(self, sa0):
 		self.bus = smbus.SMBus(1)
@@ -20,21 +21,27 @@ class SO1602A():
 		self.bus.write_byte_data(self.addr, 0x00, 0x0e)
 		self.bus.write_byte_data(self.addr, 0x00, 0x01)
 
-	def writeStr(self, str = '', line = 0):
+	def writeLine(self, str = '', line = 0):
 		if (line == 1):
 			self.bus.write_byte_data(self.addr, 0x00, (0x80 + 0x20))
 		else:
-			self.bus_write_byte_data(self.addr, 0x00, 0x01)
+			self.bus.write_byte_data(self.addr, 0x00, 0x01)
 
 		for i in range(len(str)):
 			self.bus.write_byte_data(self.addr, 0x40, ord(str[i]))
 			time.sleep(0.05)
 
 	def clearDisp(self):
-		pass
+		self.bus.write_byte_data(self.addr, 0x00, 0x01)
 
 def main():
-	pass
+	oled = SO1602A(sa0 = 0)
+	
+	oled.writeLine(str = 'Hello uhey!', line = 0)
+	time.sleep(0.5)
+	oled.writeLine(str = 'Choose ur drink', line = 1)
+	time.sleep(5)
+	oled.clearDisp()
 
 if __name__ == '__main__':
 	main()
