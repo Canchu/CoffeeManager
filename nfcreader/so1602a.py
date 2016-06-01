cf2fc2c3bc8fe6a45eb457b2493e44d925b7f26e
Yuhei Tsutsui <uhey@redimpulz.com>
2016-06-02 04:09:38 +0900

    適当に文字列出力が出来るように

--- nfcreader/so1602a.py
+++ nfcreader/so1602a.py
@@ -3,14 +3,32 @@
 
 class SO1602A():
 	"""docstring for SO1602A"""
+
+	import smbus
+	import time
+
 	def __init__(self, sa0):
+		self.bus = smbus.SMBus(1)
+
 		if (sa0 == 0):
 			self.addr = 0x3c
 		else:
 			self.addr = 0x3d
 
+		self.bus.write_byte_data(self.addr, 0x00, 0x01)
+		self.bus.write_byte_data(self.addr, 0x00, 0x02)
+		self.bus.write_byte_data(self.addr, 0x00, 0x0e)
+		self.bus.write_byte_data(self.addr, 0x00, 0x01)
+
 	def writeStr(self, str = '', line = 0):
-		pass
+		if (line == 1):
+			self.bus.write_byte_data(self.addr, 0x00, (0x80 + 0x20))
+		else:
+			self.bus_write_byte_data(self.addr, 0x00, 0x01)
+
+		for i in range(len(str)):
+			self.bus.write_byte_data(self.addr, 0x40, ord(str[i]))
+			time.sleep(0.05)
 
 	def clearDisp(self):
 		pass
