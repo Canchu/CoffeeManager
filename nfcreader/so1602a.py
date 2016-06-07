ad569836296b40c619f1e95ca80586dfe2fa0cf9
uhey <uhey@redimpulz.com>
2016-06-07 17:05:15 +0900

    必要な機能は一通り

--- nfcreader/so1602a.py
+++ nfcreader/so1602a.py
@@ -8,7 +8,7 @@ class SO1602A():
 	"""docstring for SO1602A"""
 
 
-	def __init__(self, sa0):
+	def __init__(self, sa0 = 0, cursor = False, blink = False):
 		self.bus = smbus.SMBus(1)
 
 		if (sa0 == 0):
@@ -16,27 +16,32 @@ class SO1602A():
 		else:
 			self.addr = 0x3d
 
-		# Clear Display
-		self.bus.write_byte_data(self.addr, 0x00, 0x01)
-		# Return Home
-		self.bus.write_byte_data(self.addr, 0x00, 0x02)
-		# Display On
-		self.bus.write_byte_data(self.addr, 0x00, 0x0c)
-		# Clear Display
-		self.bus.write_byte_data(self.addr, 0x00, 0x01)
+		self.clearDisplay()
+		self.returnHome()
+		self.displayOn(cursor, blink)
+		self.clearDisplay()
 
-	def writeLine(self, str = '', line = 0):
+	def writeLine(self, str = '', line = 0, align = 'left'):
+		while (len(str) < 16):
+			if (align == 'right'):
+				str = ' ' + str
+			else:
+				str = str + ' '
+
 		if (line == 1):
 			self.bus.write_byte_data(self.addr, 0x00, (0x80 + 0x20))
 		else:
-			self.bus.write_byte_data(self.addr, 0x00, 0x01)
+			self.bus.write_byte_data(self.addr, 0x00, 0x80)
 
 		for i in range(len(str)):
 			self.bus.write_byte_data(self.addr, 0x40, ord(str[i]))
 			time.sleep(0.05)
 
-	def clearDisp(self):
+	def clearDisplay(self):
 		self.bus.write_byte_data(self.addr, 0x00, 0x01)
+	
+	def returnHome(self):
+		self.bus.write_byte_data(self.addr, 0x00, 0x02)
 
 	def displayOn(self, cursor = False, blink = False):
 		cmd = 0x0c
