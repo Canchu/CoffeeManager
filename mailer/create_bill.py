a5a658e12f9570567afe6ef9681b133b44c20a55
uhey <uhey@redimpulz.com>
2016-06-19 00:48:50 +0900

    明細を作る関数の作成

--- mailer/create_bill.py
+++ mailer/create_bill.py
@@ -6,9 +6,62 @@ import json
 import MySQLdb
 from datetime import datetime
 
+from pprint import pprint
+
 SQL_SECRET_FILE = 'sql_secret.json'
 TABLE_NAME = 'test'
 TABLE_NAME_ID = 'test_id'
+TABLE_NAME_DRINKS = 'test_drinks'
+
+f = open(SQL_SECRET_FILE, 'r')
+sql_info = json.load(f)
+
+connection = MySQLdb.connect(
+	host = sql_info['host'],
+	db = sql_info['dbname'],
+	user = sql_info['user'],
+	passwd = sql_info['passwd'])
+cursor = connection.cursor()
+
+def createBill(username, year, month):
+	bill = {}
+	bill['name'] = username
+	bill['subtotal'] = {}
+
+	sql_search_date = "time >= '%04d-%02d-01' and time <= '%04d-%02d-31'" % ( year, month, year, month )
+
+	# get drinks info
+	drinks = []
+	cmd = "select name from %s;" % TABLE_NAME_DRINKS
+	cursor.execute(cmd)
+	result = cursor.fetchall()
+	for row in result:
+		drinks.append(row[0])
+
+	print drinks
+
+	for drink in drinks:
+		bill['subtotal'][drink] = [0, 0]
+		cmd = (
+			"select count(*), coalesce(sum(price), 0) from %s "
+			"where name = '%s' and item = '%s' and %s;"
+			% ( TABLE_NAME, username, drink, sql_search_date ))
+		cursor.execute(cmd)
+		result = cursor.fetchone()
+		bill['subtotal'][drink][0] = int(result[0])
+		bill['subtotal'][drink][1] = int(result[1])
+
+	bill['total'] = [0, 0]
+	cmd = (
+		"select count(*), coalesce(sum(price), 0) from %s "
+		"where name = '%s' and %s;"
+		% ( TABLE_NAME, username, sql_search_date ))
+	cursor.execute(cmd)
+	result = cursor.fetchone()
+	bill['total'][0] = int(result[0])
+	bill['total'][1] = int(result[1])
+
+	return bill
 
 def main():
 	year = datetime.now().year
@@ -17,18 +70,15 @@ def main():
 	output_filename = '%04d-%02d.csv' % ( year, month )
 	print '%d年%d月の明細' % ( year, month )
 
-	f = open(SQL_SECRET_FILE, 'r')
-	sql_info = json.load(f)
-
-	connection = MySQLdb.connect(
-		host = sql_info['host'],
-		db = sql_info['dbname'],
-		user = sql_info['user'],
-		passwd = sql_info['passwd'])
-	cursor = connection.cursor()
+	drinks = []
+	cmd = "select name from %s;" % TABLE_NAME_DRINKS
+	cursor.execute(cmd)
+	result = cursor.fetchall()
+	for row in result:
+		drinks.append(row[0])
 
 	sql = (
-		"select count(*), coalesce(sum(value), 0) from %s "
+		"select count(*), coalesce(sum(price), 0) from %s "
 		"where %s;"
 		% ( TABLE_NAME, sql_search_date ))
 	cursor.execute(sql)
@@ -43,38 +93,17 @@ def main():
 	for row in results:
 		users.append(row[0])
 
-	drinks = ['Barista', 'Dolce Gusto', 'Dolce Gusto with Milk', 'Special T', 'Iced Coffee']
-
-	# ユーザーごとに明細を作成
-	bills = {}
-	for user in users:
-		bills[user] = {}
-		for drink in drinks:
-			bills[user][drink] = []
-			sql = (
-				"select count(*), coalesce(sum(value), 0) from %s "
-				"where name = '%s' and item = '%s' and %s;"
-				% ( TABLE_NAME, user, drink, sql_search_date ))
-			cursor.execute(sql)
-			result = cursor.fetchone()
-			bills[user][drink].append(int(result[0]))
-			bills[user][drink].append(int(result[1]))
-		sql = (
-			"select count(*), coalesce(sum(value), 0) from %s "
-			"where name = '%s' and %s;"
-			% ( TABLE_NAME, user, sql_search_date ))
-		cursor.execute(sql)
-		result = cursor.fetchone()
-		bills[user]['total'] = [int(result[0]), int(result[1])]
+	bill = createBill('uhey', 2016, 5)
+	pprint(bill)
 
 	cursor.close()
 	connection.close()
 
-	for user in users:
-		print '%sさんのご利用明細' % user
-		for drink in drinks:
-			print '%s: %d杯 %d円' % ( drink, bills[user][drink][0], bills[user][drink][1] )
-		print '合計: %d杯 %d円' % ( bills[user]['total'][0], bills[user]['total'][1] )
+	# for user in users:
+	# 	print '%sさんのご利用明細' % user
+	# 	for drink in drinks:
+	# 		print '%s: %d杯 %d円' % ( drink, bills[user][drink][0], bills[user][drink][1] )
+	# 	print '合計: %d杯 %d円' % ( bills[user]['total'][0], bills[user]['total'][1] )
 
 if __name__ == '__main__':
 	main()
