0aa78a0e4ea26070f48041a491602b2556afc59d
uhey <konbu.su@gmail.com>
2016-06-18 20:28:04 +0900

    メール送信機能・明細作成

---
+++ mailer/create_bill.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+#coding: utf-8
+
+import os
+import json
+import MySQLdb
+
+SQL_SECRET_FILE = 'sql_secret.json'
+TABLE_NAME = 'test_id'
+
+def main():
+	f = open(SQL_SECRET_FILE, 'r')
+	sql_info = json.load(f)
+
+	connection = MySQLdb.connect(
+		host = sql_info['host'],
+		db = sql_info['dbname'],
+		user = sql_info['user'],
+		passwd = sql_info['passwd'])
+	cursor = connection.cursor()
+
+	# ユーザー一覧を取得
+	sql = 'select name from %s;' % TABLE_NAME
+	cursor.execute(sql)
+	results = cursor.fetchall()
+	users = []
+	for row in results:
+		users.append(row[0])
+
+	drinks = ['Barista', 'Dolce Gusto', 'Dolce Gusto with Milk', 'Special T', 'Iced Coffee']
+
+	# ユーザーごとに明細を作成
+	bills = {}
+	for user in users:
+		bills[user] = {}
+		for drink in drinks:
+			bills[user][drink] = []
+			sql = "select count(*), coalesce(sum(value), 0) from test where name = '%s' and item = '%s';" % ( user, drink )
+			cursor.execute(sql)
+			results = cursor.fetchall()
+			for row in results:
+				bills[user][drink].append(int(row[0]))
+				bills[user][drink].append(int(row[1]))
+		sql = "select count(*), coalesce(sum(value), 0) from test where name = '%s';" % user
+		cursor.execute(sql)
+		result = cursor.fetchone()
+		bills[user]['total'] = [int(result[0]), int(result[1])]
+
+	cursor.close()
+	connection.close()
+
+	for user in users:
+		print '%sさんのご利用明細' % user
+		for drink in drinks:
+			print '%s: %d杯 %d円' % ( drink, bills[user][drink][0], bills[user][drink][1] )
+		print '合計: %d杯 %d円' % ( bills[user]['total'][0], bills[user]['total'][1] )
+
+if __name__ == '__main__':
+	main()
