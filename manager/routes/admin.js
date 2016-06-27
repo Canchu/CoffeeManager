201c19bcdf7bb7f2eb5385741df44dd1831eaabb
uhey <uhey@redimpulz.com>
2016-06-28 01:22:50 +0900

    ユーザー一覧表示してみた

--- manager/routes/admin.js
+++ manager/routes/admin.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var async = require('async');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'user',
+	password: 'NojiNoji',
+	database: 'CoffeeManager_db'
+});
+
+var table_id = "test_id";
+
+connection.connect(function(err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('DBconnected as id ' + connection.threadId);
+});
 
 router.get('/', function(req, res, next) {
-	res.render('admin', {});
+	var params = {};
+	var users = [];
+	var sql = "SELECT * FROM " + table_id + ";";
+	connection.query(sql, function(err, rows) {
+		if (err) throw err;
+		params.users = rows;
+		console.log(params);
+		res.render('admin', params);
+	});
 });
 
 module.exports = router;
